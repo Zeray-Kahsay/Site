299df4f4110a2b20ef98a0b1ab7630e2082decc4
Zeray-Kahsay <kahsayzeray@gmail.com>
2025-06-27 06:41:42 +0200

    items pagination added

--- Site.API/Interfaces/IITemRepository.cs
+++ Site.API/Interfaces/IITemRepository.cs
@@ -1,11 +1,13 @@
 using System;
 using Site.API.DTOs;
 using Site.API.Entities.Item;
+using Site.API.RequestHelpers;
 
 namespace Site.API.Interfaces;
 
 public interface IITemRepository
 {
   Task<IEnumerable<ItemDto>> GetItemsAsync(int? typeId, int? categoryId);
+  Task<IEnumerable<ItemDto>> GetPaginatedItemsAsync(ItemParams itemParams);
   Task<Item?> GetItemByIdAsync(int id);
 }
